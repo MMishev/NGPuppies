292557b02787f13c8ce66444b6a7b9fd92d3f730
Mishev <m.n.mishev@gmail.com>
2018-08-28 13:19:53 +0300

    added project base

---
+++ src/main/java/com/ngpuppies/ngpuppies/NgpuppiesApplication.java
@@ -0,0 +1,12 @@
+package com.ngpuppies.ngpuppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgpuppiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgpuppiesApplication.class, args);
+    }
+}
